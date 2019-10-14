e79657d7a58f6e44364d5765f592df885564a416
Pete Lane <pajlane@protonmail.com>
2019-10-14 10:11:15 -0700

    Add project files.

---
+++ SellerASINScraper/Excel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Office.Interop.Excel;
+using _Excel = Microsoft.Office.Interop.Excel;
+
+namespace SellerASINScraper
+{
+    class Excel
+    {
+        string path = "";
+        _Application excel = new _Excel.Application();
+        Workbook wb;
+        Worksheet ws;
+
+        public Excel()
+        {
+
+        }
+
+        public void CreateNewFile()
+        {
+            this.wb = excel.Workbooks.Add(XlWBATemplate.xlWBATWorksheet);
+            this.ws = wb.Worksheets[1];
+        }
+
+        public void CreateNewSheet()
+        {
+            Worksheet tempsheet = wb.Worksheets.Add(After: ws);
+
+        }
+
+        public Excel(string path, int Sheet)
+        {
+            this.path = path;
+            wb = excel.Workbooks.Open(path);
+            ws = wb.Worksheets[Sheet];
+        }
+
+        public void WriteToCell(int i, int j, string s)
+        {
+            //i++;
+            //j++;
+            ws.Cells[i, j].Value2 = s;
+        }
+
+        public void Save()
+        {
+            wb.Save();
+        }
+
+        public void SaveAs(string path)
+        {
+            wb.SaveAs(path)
+;        }
+
+        public void Quit()
+        {
+            excel.Quit();
+        }
+
+        //testcomment
+    }
+}
---
+++ SellerASINScraper/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+
+
+namespace SellerASINScraper
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private bool IsElementPresent(By by, IWebDriver driver) 
+        {                                                       
+            try
+            {
+                driver.FindElement(by);
+                return true;
+            }
+            catch (NoSuchElementException)
+            {
+                return false;
+            }
+            catch (ElementNotVisibleException)
+            {
+                return false;
+            }
+
+        }
+        public void Run_Click(object sender, RoutedEventArgs e) //Run button!
+        {
+
+            Close();
+            var driverService = ChromeDriverService.CreateDefaultService(@"\\brmpro\MACAPPS\ClickOnce\CustomerServiceAutomationTool");
+            driverService.HideCommandPromptWindow = true;
+            var driver = new ChromeDriver(driverService, new ChromeOptions());
+            
+            string sellerID = SellerIDx.Text;
+
+            driver.Url = "https://www.amazon.com/s?me=" + sellerID + "&rh=p_4%3ABob%27s+Red+Mill";
+
+
+            Excel excel = new Excel();
+            excel.CreateNewFile();
+            excel.CreateNewSheet();
+           
+
+            var cellCount = 1;
+            var pageItemCount = 1;
+            var totalItemCount = 1; //exists because pageItemCount will be reset every new page
+            var pageNumber = 2;
+
+            while (IsElementPresent(By.XPath("//*[@id='search']/div[1]/div[2]/div/span[7]/div/span/div/div/ul/li[" + pageNumber + "]"), driver) == true)
+            { 
+
+                while (IsElementPresent(By.XPath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[1]/div[" + pageItemCount + "]"), driver) == true)
+                {
+                    string result = driver.FindElement(By.XPath("//*[@id='search']/div[1]/div[2]/div/span[3]/div[1]/div[" + pageItemCount + "]")).GetAttribute("data-asin");
+                    excel.WriteToCell(cellCount, 1, result);
+
+                    cellCount++;
+                    totalItemCount++;
+                    pageItemCount++;
+                }
+
+
+                pageItemCount = 1;
+                pageNumber++;
+
+                if (IsElementPresent(By.XPath("//*[@id='search']/div[1]/div[2]/div/span[7]/div/span/div/div/ul/li[" + pageNumber + "]"), driver) == true)
+                    {
+                    var clickPG = driver.FindElement(By.XPath("//*[@id='search']/div[1]/div[2]/div/span[7]/div/span/div/div/ul/li[" + pageNumber + "]"));
+                    clickPG.Click();
+                    }
+
+            }
+
+
+            excel.SaveAs(@"ASIN Report for " + sellerID);
+            excel.Quit();
+            driver.Quit();
+        }
+    }
+}
